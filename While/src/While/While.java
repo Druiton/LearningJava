6429e7035a170cdaa5ea38b72743acd1d943e573
Maxim <smn200@mail.ru>
2022-04-24 17:59:30 +1000

    While

---
+++ While/src/While/While.java
@@ -0,0 +1,12 @@
+package While;
+
+public class While {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a != 0) {
+            System.out.println("Обратный отсчет " + a);
+            a--;
+        }
+        System.out.println("Отсчет окончен");
+    }
+}
